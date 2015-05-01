13caa7f6f4228a7609e6abf92b376a1e7cffc62b
DoumanAsh <custparasite@gmx.se>
2015-05-01 14:30:04 +0300

    Re-design docs

---
+++ doc/search-index.js
@@ -0,0 +1,3 @@
+var searchIndex = {};
+searchIndex['trace_macros'] = {"items":[[0,"","trace_macros","A crate with printing macros.",null,null],[14,"WARNING!","","WARNING macro",null,null],[14,"ERROR!","","ERROR macro",null,null],[14,"INFO!","","INFO macro",null,null],[14,"DEBUG!","","DEBUG macro",null,null],[14,"ENTER!","","ENTER macro",null,null],[14,"TRACE!","","Main trace macro.",null,null],[14,"strace!","","Simplified trace macro",null,null]],"paths":[]};
+initSearch(searchIndex);
